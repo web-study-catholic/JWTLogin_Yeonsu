c01585dfbb8c72b1c7525cf78f83184e8a755893
Yeon su <dustn2366@naver.com>
2023-06-01 15:09:25 +0900

    fix: Login 수정

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,9 @@ function getToken() {
 export default function Home() {
   const [username, setUsername] = useState("");
 
+  //컴포넌트가 마운트된 후 한번 실행
+  //getToken을 호출해 토큰의 존재 여부를 확인하고 토큰이 있을 경우에만 username 상태를
+  //업데이트하고 화면에 사용자가 누군지 보여줌
   useEffect(() => {
     const token = getToken();
     if(token){
@@ -16,6 +19,7 @@ export default function Home() {
   },[]);
 
   return(
+    // 조건부로 렌더링했음
     <div>
       <h1>HOME</h1>
       {username? (
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,15 @@ import { useNavigate } from 'react-router-dom';
 
 function Login() {
   const [input, setInput] = useState({username : "", password : ""});
-  const [Loding, setLoding] = useState(false);
+  //const [Loding, setLoding] = useState(false);
   const navigate = useNavigate();
 
+  //인풋 필드의 변경 input상태를 업데이트하고 'name'속성을 사용해 해당 필드값을 업데이트함
   function handleOnchange(e) {
     setInput((input) => ({...input, [e.target.name] : e.target.value}));
   }
 
+  //
   function handleSubmit(e) {
     e.preventDefault();
     autoLogin();
@@ -17,26 +19,19 @@ function Login() {
 
   const hardCodedToken = 'eyJhbCI6IkhTMjU2IiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VybmFtZSI6ImR1c3RuMTIzIiwicGFzc3dvcmQiOjEyMzR9.qyHjEn3Lav_MzOix1H4bBM84dWPDcnTrSyl6G2zOEOc';
   
+  //input상태에서 사용자와 비밀번호를 가지고 와 입력된 값이 일치하는지 확인함
+  //일치하는 경우 토큰을 로컬 스토리지에 저장하고 네비게이트를 이용해 home으로 이동
+
   function autoLogin() {
     const {username , password} = input;
     if(username === 'dustn123' && password === '1234'){
       const hardCodedToken = 'eyJhbCI6IkhTMjU2IiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJ1c2VybmFtZSI6ImR1c3RuMTIzIiwicGFzc3dvcmQiOjEyMzR9.qyHjEn3Lav_MzOix1H4bBM84dWPDcnTrSyl6G2zOEOc';
       localStorage.setItem('authToken', hardCodedToken);
-      setLoding(true);
+      //setLoding(true);
       navigate('/');
     } else{
-      setLoding(false);
+      //setLoding(false);
       console.log("로그인 실패"); 
-  }
-  
-  // 로컬 스토리지에서 토큰을 가져옴
-  const storedToken = localStorage.getItem('authToken');
-
-  // 저장된 토큰이 임의의 토큰과 일치하는지 확인
-  if (storedToken === hardCodedToken) {
-      console.log('Automatically logged in');
-  } else {
-      console.log('Token is not valid');
   }
 }
   return (
